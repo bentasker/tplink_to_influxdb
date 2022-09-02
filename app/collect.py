123a40290e333c466eb10eb5060f6c50b4252db2
B Tasker <github@bentasker.co.uk>
2022-09-02 23:38:26 +0100

    Add support for polling kasa devices

--- app/collect.py
+++ app/collect.py
@@ -55,6 +55,15 @@ def main():
             )
             influxes.append(c)
 
+    for kasa in config["kasa"]["devices"]:
+        now_usage_w, today_usage = poll_kasa(kasa['ip'])
+        if not now_usage_w:
+            print(f"Failed to communicate with device {kasa['ip']}")
+            continue
+        
+        print(f"Plug: {kasa['name']} using {now_usage_w}W, today: {today_usage/1000} Wh")
+
+
     for tapo in config["tapo"]["devices"]:
         now_usage_w, today_usage = poll_tapo(tapo['ip'], config["tapo"]["user"], config["tapo"]["passw"])
         if not now_usage_w:
@@ -62,7 +71,33 @@ def main():
             continue
         
         print(f"Plug: {tapo['name']} using {now_usage_w}W, today: {today_usage/1000} Wh")
+
         
+def poll_kasa(ip):
+    ''' Poll a TP-Link Kasa smartplug
+    
+    TODO: need to add some exception handling to this
+    '''
+    p = SmartPlug(ip)
+    asyncio.run(p.update())
+    
+    usage_dict = p.emeter_realtime
+    today_usage = p.emeter_today
+    
+    # emeter_today seems to be quite racey, sometimes returning 0 sometimes returning 0.001
+    # wh usage since it was powered on (at time of writing) is 80.
+    # 
+    # probably better to just omit the metric for now.
+    #
+    # It _may_ be because I've blocked the device from WAN access - https://github.com/home-assistant/core/issues/45436#issuecomment-766454897
+    # sounds like it needs NTP access to be able to track this.
+    #
+    # Allowing UDP 123 out seems to have resolved it
+    #today_usage = False
+    now_usage_w = usage_dict["power_mw"] / 1000
+
+    return now_usage_w, today_usage
+
 
 def poll_tapo(ip, user, passw):
     ''' Poll a TP-Link Tapo smartplug
