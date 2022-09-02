243669c92a99689773198123ed800a33b1122ba8
B Tasker <github@bentasker.co.uk>
2022-09-02 23:35:02 +0100

    Add support for polling Tapo devices

--- app/collect.py
+++ app/collect.py
@@ -2,7 +2,7 @@
 #
 #
 #
-# pip install pyyaml influxdb-client
+# pip install pyyaml influxdb-client PyP100 python-kasa
 
 import asyncio
 import os
@@ -11,6 +11,8 @@ import yaml
 import influxdb_client
 
 from influxdb_client.client.write_api import SYNCHRONOUS
+from kasa import SmartPlug
+from PyP100 import PyP110
 
 
 
@@ -53,7 +55,34 @@ def main():
             )
             influxes.append(c)
 
+    for tapo in config["tapo"]["devices"]:
+        now_usage_w, today_usage = poll_tapo(tapo['ip'], config["tapo"]["user"], config["tapo"]["passw"])
+        if not now_usage_w:
+            print(f"Failed to communicate with device {tapo['ip']}")
+            continue
         
+        print(f"Plug: {tapo['name']} using {now_usage_w}W, today: {today_usage/1000} Wh")
+        
+
+def poll_tapo(ip, user, passw):
+    ''' Poll a TP-Link Tapo smartplug
+    '''
+    
+    try:
+        p110 = PyP110.P110(ip, user, passw)
+        p110.handshake() #Creates the cookies required for further methods
+        p110.login() #Sends credentials to the plug and creates AES Key and IV for further methods        
+        usage_dict = p110.getEnergyUsage()
+    except:
+        return False, False
+
+    today_usage = usage_dict["result"]["today_energy"]
+    now_usage_w = usage_dict["result"]["current_power"] / 1000
+                                                    
+    return now_usage_w, today_usage
+    
+
+
 
 
 if __name__ == "__main__":
