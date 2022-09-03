36e765825fd7e17432c26635aa2e71f5d57dc564
B Tasker <github@bentasker.co.uk>
2022-09-03 12:35:13 +0100

    Update example

--- example/invoke.sh
+++ example/invoke.sh
@@ -4,4 +4,5 @@
 #
 #
 
-docker run --rm --name="tplink_to_influxdb" -v $PWD/config.yml:/config.yml bentasker12/tplink_to_influxdb:0.1
+docker pull bentasker12/tplink_to_influxdb:latest
+docker run --rm --name="tplink_to_influxdb" -v $PWD/config.yml:/config.yml bentasker12/tplink_to_influxdb:latest
